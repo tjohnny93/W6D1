4fc2aa91cf30582994ba3cac7e172bb7fac9eaeb
tjohnny93 <tjohnny93@berkeley.edu>
2019-06-17 13:55:24 -0700

    complete sum with arguments and rest operator

--- arguments_exercise/arguments_exercise.js
+++ arguments_exercise/arguments_exercise.js
@@ -1,8 +1,18 @@
-function sum(arguments){
-  debugger;
+function sum(){
   let sum = 0;
   Object.values(arguments).forEach(function(ele){
     sum += ele;
   });
   return sum;
 }
+
+sum(2,2,3);
+
+function sumRest(...arguments){
+  let sum = 0;
+  (arguments).forEach(function(ele){
+    sum+=ele;
+  });
+  return sum;
+}
+console.log(sumRest(2,2,3));
